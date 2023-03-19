e3fcfd3719a330da0b84725eb6abd264f245696f
unknown <facundocolavitte@gmail.com>
2023-03-18 22:41:43 -0300

    SW

--- Unidirec_comm_satelites/main/Drivers/lib/API_E22.h
+++ Unidirec_comm_satelites/main/Drivers/lib/API_E22.h
@@ -8,13 +8,23 @@
 #ifndef MAIN_DRIVERS_LIB_API_E22_H_
 #define MAIN_DRIVERS_LIB_API_E22_H_
 
+/********************** inclusions *******************************************/
+
 #include "stdio.h"
 
-#define OPCODE_SETTX (uint8_t)0x83
+/********************** macros and defines ***********************************/
+
+/*** OPCODES ***/
+#define OPCODE_SETTX			(uint8_t)0x83
+#define OPCODE_SETSTANDBY		(uint8_t)0x80
+#define OPCODE_SETSLEEP			(uint8_t)0x84
+#define OPCODE_WRITEREGISTER	(uint8_t)0x0D
+#define OPCODE_READREGISTER		(uint8_t)0x1D
+#define OPCODE_WRITEBUFFER		(uint8_t)0x0E
+#define OPCODE_READBUFFER		(uint8_t)0x1E
 
 
-
-
+/*** GPIO ***/
 #define GPIO_SPI_MOSI 23
 #define GPIO_SPI_MISO 19
 #define GPIO_SPI_SCK  18
@@ -22,5 +32,15 @@
 #define GPIO_E22_BUSY 21
 #define GPIO_E22_RST  22
 
+/********************** typedef **********************************************/
+
+/********************** external data declaration ****************************/
+
+/********************** external functions declaration ***********************/
+
+
+
 
 #endif /* MAIN_DRIVERS_LIB_API_E22_H_ */
+
+/********************** end of file ******************************************/
--- Unidirec_comm_satelites/main/Drivers/src/API_E22.c
+++ Unidirec_comm_satelites/main/Drivers/src/API_E22.c
@@ -4,14 +4,29 @@
  *  Created on: 11 mar. 2023
  *      Author: Facu
  */
+
 #include "API_E22.h"
 #include <API_Time.h>
+
+/********************** inclusions *******************************************/
+
+/********************** macros and definitions *******************************/
+
+/********************** internal data declaration ****************************/
+
+/********************** internal functions declaration ***********************/
+
+/********************** external functions declaration ***********************/
 
 extern void HAL_SPI_write(uint8_t *SPI_message, uint8_t message_lenght);
 extern void HAL_GPIO_write(uint8_t GPIO, uint8_t state);
 extern void HAL_GPIO_read(uint8_t GPIO);
 
+/********************** internal data definition *****************************/
 
+/********************** external data definition *****************************/
+
+/********************** internal functions definition ************************/
 
 void _E22_write_command(uint8_t opcode, uint8_t *p_parameters, uint8_t n_parameters){
 	/*PSEUDOCódigo: */
@@ -21,6 +36,8 @@ void _E22_write_command(uint8_t opcode, uint8_t *p_parameters, uint8_t n_paramet
 	//HAL_SPI_write(SPI_message);	/*Declarar función en el E22_port_ESP32*/
 }
 
+/********************** external functions definition ************************/
+
 void E22_setTx(uint32_t timeOut){
 	uint8_t parameters[3];
 	parameters[0] =(uint8_t)((timeOut >> 16) & 0xFF);
@@ -29,7 +46,7 @@ void E22_setTx(uint32_t timeOut){
 
 	uint8_t n_parameters = 3;
 
-	_E22_write_command(OPCODE_SETTX,parameters,n_parameters);
+	_E22_write_command(OPCODE_SETTX, parameters, n_parameters);
 }
 
 void E22_reset(void){
@@ -39,6 +56,44 @@ void E22_reset(void){
 	vTaskDelay(10/ portTICK_PERIOD_MS);
 }
 
+
+/**
+ *	@note	Con "payloadlength" se define la cantidad de bytes a mandar en modo Tx
+ *			La función TxBaseAddr() define el inicio desde donde se comienza a transmitir o la función SetBufferBaseAddresses() que define también Rx
+ */
 void E22_SetPacketParams(void/* ??? */){
 
 }
+
+/**
+ *	@note	El buffer de 256 bytes se borra al poner el E22 en modo SLEEP
+ */
+void E22_WriteBuffer(uint8_t offset, uint8_t *data_buffer, uint8_t data_lenght){
+	uint8_t parameters[20];
+	uint8_t n_parameters = 1+data_lenght;
+	//parameters = 1 byte de offset + n bytes de data
+	_E22_write_command(OPCODE_WRITEBUFFER, parameters, n_parameters);
+}
+
+void E22_ReadBuffer(uint8_t offset, uint8_t *data_buffer){
+	uint8_t *parameters = &offset;
+	uint8_t n_parameters = 1;
+	_E22_write_command(OPCODE_SETTX, parameters, n_parameters);
+	//Lo que devuelve, escribirlo en data_buffer
+}
+
+/**
+ *	@note	Si el modulo E22 tiene el pin BUSY en bajo está preparado para recibir un comando.
+ *			Si el pin busy está en alto se debe esperar para enviar un nuevo comando
+ *			Al terminar de enviar un comando hay un retardo hasta que el pin busy se pone en alto para indicar que está procesando el comando
+ *			El tiempo de retardo del BUSY es de hasta 126us, excepto cuando sale del modo SLEEP que puede tardar hasta 3,5ms
+ */
+bool E22_check_busy_pin_status(){
+	return 1;
+}
+
+void E22_GetRxBufferStatus(){
+
+}
+
+/********************** end of file ******************************************/
